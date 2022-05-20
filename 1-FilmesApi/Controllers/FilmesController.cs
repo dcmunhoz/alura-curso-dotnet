b241902a30734b88692734d4da740f1b0751aca5
dcmunhoz <dc.munhoz@hotmail.com>
2022-05-19 21:18:09 -0300

    Primeiras rotas

---
+++ 1-FilmesApi/Controllers/FilmesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesApi.Models;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController: ControllerBase
+    {
+
+        public static List<Filme> filmes = new();
+        public static int count = 0;
+
+        [HttpGet]
+        public List<Filme> ListaFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpPost]
+        public Filme AdicionarFilme([FromBody] Filme filme)
+        {
+            count++;
+            filme.Id = count; 
+            filmes.Add(filme);
+
+            return filme;
+
+        }
+
+        [HttpGet("{id}")]
+        public Filme BuscarFilme(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
+        }
+
+    }
+}
---
+++ 1-FilmesApi/Models/Filme.cs
@@ -0,0 +1,13 @@
+﻿namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Diretor { get; set; }
+        public int Duracao { get; set; }
+
+
+    }
+}
