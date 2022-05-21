b0c0ace7b40b3745c24f4faa7edaf0087765b97c
dcmunhoz <dc.munhoz@hotmail.com>
2022-05-21 11:44:09 -0300

    Parametros obrigatórios

--- 1-FilmesApi/Models/Filme.cs
+++ 1-FilmesApi/Models/Filme.cs
@@ -1,11 +1,17 @@
-﻿namespace FilmesApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
 {
     public class Filme
     {
         public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
         public string Descricao { get; set; }
+        [Required]
         public string Diretor { get; set; }
+        [Required]
+        [Range(1, 999)]
         public int Duracao { get; set; }
 
 
